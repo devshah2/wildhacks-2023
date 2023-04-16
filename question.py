2fe55f99bc71fa92ed4378e88e8eaa8a95ef8bf6
Kevin Hayes <KevinHayes2026@u.northwestern.edu>
2023-04-16 04:30:01 -0500

    Upvote Basics

--- question.py
+++ question.py
@@ -1,11 +1,23 @@
 
+import threading
+
 class Question:
 
-    def __init__(self, data, votes = 0):
+    def __init__(self, uuid, data, votes = 0):
+        self.uuid = uuid
         self.data = data
         self.votes = votes
+        self.mut = threading.Lock()
 
     def get_data(self):
-        return data
+        return self.data
     def get_votes(self):
-        return votes
+        return self.votes
+    def votes_increment(self):
+        self.mut.acquire()
+        self.votes += 1
+        self.mut.release()
+    def votes_decrement(self):
+        self.mut.acquire()
+        self.votes -= 1
+        self.mut.release()
