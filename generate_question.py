1fdc6f1a8658a7233546c232b64465775e16ba87
devshah2 <35283231+devshah2@users.noreply.github.com>
2023-04-16 04:18:29 -0500

    generate_question bug

--- generate_question.py
+++ generate_question.py
@@ -3,8 +3,10 @@ import question
 import cite_transcript_model
 
 def update_questions(transcript, questions, temperature, class_lvl):
-    generated = tai3_5.get_questions(class_lvl,temperature,transcript.get_window_bytes().decode())
+    generated = tai3_5.get_questions(transcript.get_window_bytes().decode(),class_lvl,temperature)
+    print(generated)
     if(generated!=""):
         answer = cite_transcript_model.cite_transcript(transcript.get_full(),generated)
+        print(answer)
         questions.append(question(generated,0,answer))
     
