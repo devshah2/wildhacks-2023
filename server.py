d8990b0de3329d0357c01dacc89b88c9e89314ea
Kevin Hayes <KevinHayes2026@u.northwestern.edu>
2023-04-16 02:15:20 -0500

    403

--- server.py
+++ server.py
@@ -4,7 +4,8 @@ import tai
 from transcript import Transcript
 
 app = fk.Flask(__name__)
-app.secret_key = "holyshitthisisunsafesomeonegeneratearealsecretkeybutimtoolazysonotme"
+app.config['SECRET_KEY'] = str(pyuuid.uuid4().hex)
+app.config['SESSION_PERMANENT'] = False
 
 # Kind of arbitrary but this is a safe number for testing
 WINDOW_SIZE = 4096
@@ -17,7 +18,8 @@ def generate_questions(transcript):
 
 @app.route("/", methods=["GET"])
 def index():
-    if 'prof' in fk.session:
+    print(fk.session)
+    if 'prof' in fk.session and fk.session['prof']:
         return fk.redirect(fk.url_for("professor"))
     else:
         return fk.redirect(fk.url_for("student"))
@@ -31,7 +33,7 @@ def access_key():
         fk.session['prof'] = True
         return fk.redirect(fk.url_for('professor'))
     else:
-        return fk.redirect(fk.url_for('its_just_a_prank_bro'))
+        fk.abort(403)
 
 @app.route("/professor")
 def professor():
@@ -41,10 +43,6 @@ def professor():
 def student():
     return fk.render_template("student_view_bootstrap.html")
 
-@app.route("/its_just_a_prank_bro", methods=["GET"])
-def its_just_a_prank_bro():
-    return fk.render_template("scary_page.html")
-
 @app.errorhandler(500)
 def errorhandler_500(error):
     return fk.render_template("500.html"), 500
@@ -53,11 +51,15 @@ def errorhandler_500(error):
 def errorhandler_404(error):
     return fk.render_template("404.html"), 404
 
+@app.errorhandler(403)
+def errorhandler_403(error):
+    return fk.render_template("403.html"), 403
+
 def main():
     # Start threads here
 
     # 
-    app.run()
+    app.run(host='0.0.0.0',port=5000)
 
 
 if __name__ == "__main__":
