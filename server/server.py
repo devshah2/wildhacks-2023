9c57a0e8729a52302fefdfcdfdc118161d8b6c6d
Kevin Hayes <KevinHayes2026@u.northwestern.edu>
2023-04-15 20:15:25 -0500

    More work, too lazy to comment well

--- server/server.py
+++ server/server.py
@@ -1,9 +1,54 @@
+import flask as fk
+import uuid
+import json
+import jsonpickle as jsp
+import markupsafe as mks
 
-from flask import Flask
+app = fk.Flask(__name__)
+app.secret_key = "holyshitthisisunsafesomeonegeneratearealsecretkeybutimtoolazysonotme"
 
-app = Flask(__name__)
+def generate_questions(transcript):
+    return list(["How does that work?"])
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=["GET"])
+def index():
+    if not 'uuid' in fk.session:
+        # Initialize the session
+        init_session()
+    return fk.redirect(fk.url_for('session'))
+
+def init_session():
+    fk.session['uuid'] = uuid.uuid4()
+    fk.session['transcript'] = ''
+    fk.session['transcript_changed'] = True
+    fk.session['questions'] = []
+
+    return fk.redirect(fk.url_for('session'))
+
+@app.route("/session", methods=["GET"])
+def session():
+    if not 'uuid' in fk.session:
+        return "<p>Session Not Found!</p>", 404
+    uuid = fk.session['uuid']
+    if fk.session['transcript_changed']:
+        fk.session['questions'] = generate_questions(fk.session['transcript'])
+        fk.session['transcript_changed'] = False
+    questions: list[str] = fk.session['questions']
+    return fk.render_template(
+            'session_bootstrap.html', 
+            questions=questions,
+            uuid=uuid)
+
+@app.route("/append_transcript", methods=["POST"])
+def append_transcript():
+    addition = fk.request.form['addition']
+    if not 'transcript' in fk.session:
+        fk.session['transcript'] = addition
+    else:
+        fk.session['transcript'] = fk.session['transcript'].join(addition)
+    return fk.redirect(fk.url_for('session'))
+
+@app.errorhandler(404)
+def errorhandler_404(error):
+    return fk.render_template("404.html"), 404
 
---
+++ server/test_usage.py
@@ -0,0 +1,4 @@
+from cite_transcript_model import cite_transcript as ct
+
+print(ct("Dev Shah is a comp sci major at northwestern. Kevin is also a comp sci major. Lucas is unfortunately a comp eng major. Sorting is important to the world","Who is a comp sci major?"))
+print(ct("Dev Shah is a comp sci major at northwestern. Kevin is also a comp sci major. Lucas is unfortunately a comp eng major. Sorting is important to the world","Who is a comp eng major?"))
