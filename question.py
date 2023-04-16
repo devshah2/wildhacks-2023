e829bc66c0b10666c729aa329284019e316175c8
Kevin Hayes <KevinHayes2026@u.northwestern.edu>
2023-04-16 03:44:26 -0500

    Integration Hell

---
+++ question.py
@@ -0,0 +1,11 @@
+
+class Question:
+
+    def __init__(self, data, votes = 0):
+        self.data = data
+        self.votes = votes
+
+    def get_data(self):
+        return data
+    def get_votes(self):
+        return votes
